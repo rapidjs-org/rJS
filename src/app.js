63272c60b5857bed133de5eab63f271e6414a618
t-ski <t.schiepanski@gmx.de>
2021-03-04 20:44:33 +0100

    Create basic respond method

--- src/app.js
+++ src/app.js
@@ -13,12 +13,27 @@ const http = require(webConfig.useHttps ? "https" : "http");
 
 //console.log(http.STATUS_CODES[200])
 
+function respond(res, status, message) {
+	// Retrieve default message of status code if none given
+	!message && (message = http.STATUS_CODES[status]);
+
+	res.statusCode = status;
+
+	res.end(message);
+}
+
 /**
  * Handle a single request.
  * @param {*} req Request object
  * @param {*} res Response object
  */
 function handleRequest(req, res) {
+	if(req.url.length > webConfig.maxUrlLength) {
+		respond(res, 414);
+
+		return;
+	}
+
 	// ...
 	return [req, res];
 }
