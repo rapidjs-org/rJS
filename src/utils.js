6f73296364504329fc0eb9a1093a5e6407987f0d
t-ski <t.schiepanski@gmx.de>
2021-03-08 23:43:02 +0100

    Create caching module and implement functionality for get requests

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,20 @@
 module.exports = {
-    isString: value => {
-        return typeof value === "string" || value instanceof String;
-    }
+
+	/**
+     * Check whether the given value is of type string.
+     * @param {*} value Value to check
+     * @returns {Booelan} Whether the value is of type string
+     */
+	isString: value => {
+		return typeof value === "string" || value instanceof String;
+	},
+
+	/**
+     * Get the current timestamp in seconds representation.
+     * @returns {Number} Timestamp floored to seconds
+     */
+	getTimestampInSecs: _ => {
+		return Math.floor(Date.now() / 1000);
+	}
+
 };
