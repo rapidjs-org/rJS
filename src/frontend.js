fea7b69b68d59144e87ec7cc3fec8f8c23f5d9e8
t-ski <t.schiepanski@gmx.de>
2021-03-30 22:14:17 +0200

    Update feature frontend module set up

---
+++ src/frontend.js
@@ -0,0 +1,19 @@
+/**
+ * Perform a POST request.
+ * @param {String} url Endpoint URL
+ * @param {Object} body Body object to send along
+ * @returns {Promise} Request promise eventualy resolving to response on success
+ */
+module.post = function(url, body) {
+	return fetch(url, {
+		method: "POST",
+		mode: "same-origin",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		redirect: "follow",
+		referrerPolicy: "no-referrer",
+		body: JSON.stringify(body)
+	});
+}
