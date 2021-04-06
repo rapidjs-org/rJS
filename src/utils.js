cfa135c8b0be9c345bcc191db458317ed691a064
t-ski <t.schiepanski@gmx.de>
2021-04-06 03:23:48 +0200

    Separate read function to call custom reader from otuside of core module too

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,11 @@
 module.exports = {
 
-	/**
-     * Check whether the given value is of type string.
-     * @param {*} value Value to check
-     * @returns {Booelan} Whether the value is of type string
-     */
 	isString: value => {
 		return typeof value === "string" || value instanceof String;
-	}
+	},
+     
+     isFunction: value => {
+          return value && {}.toString.call(value) === "[object Function]";
+     }
 
 };
