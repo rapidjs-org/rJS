d42885a5f9caf4ce1ff674eaecc016292ecbac7c
t-ski <t.schiepanski@gmx.de>
2021-06-07 21:13:48 +0200

    Enhance caching models by slitting client and server application scope

--- src/support/cache.js
+++ src/support/cache.js
@@ -1,62 +1,64 @@
 const storage = new Map();
 
-// Frequency of cache refreshing in ms (no refreshing if value is false)
-let _cacheRefreshFrequency;
-
-/**
- * Check if the cache holds an entry for the requested URL (pathname as key).
- * Clears outdated entries implicitly resulting in a false return value.
- * @param {String} key Unique key (normalize URLs)
- * @returns {Boolean} Whether the cache holds an entry for the requested URL
- */
-function has(key) {
-	if(!storage.has(key)) {
-		return false;
+function checkEmpty(key, cacheRefreshFrequency) {
+	if(!cacheRefreshFrequency) {
+		return true;
 	}
-	if(_cacheRefreshFrequency && (storage.get(key).time + (_cacheRefreshFrequency || Math.infiniti)) < Date.now()) {
+
+	if((storage.get(key).time + (cacheRefreshFrequency || Math.infiniti)) < Date.now()) {
 		storage.delete(key);
-        
-		return false;
+		
+		return true;
 	}
 
-	return true;
+	return false;
 }
 
-/**
- * Read data associated with the given URL key from cache.
- * @param {String} key Unique key (normalize URLs)
- * @returns {String|Buffer} Data
- */
-function read(key) {
-	if(!storage.has(key)) {
-		return undefined;
-	}
-
-	return storage.get(key).data;
-}
-
-/**
- * Write given data to the cache associated with the provided URL key.
- * @param {String} key Unique key (normalize URLs)
- * @param {String|Buffer} data Data to write to cache
- */
-function write(key, data) {
-	if(_cacheRefreshFrequency == 0) {
-		return;
-	}
-
-	storage.set(key, {
-		time: Date.now(),
-		data: data
-	});
-}
-
-module.exports = cacheRefreshFrequency => {
-	_cacheRefreshFrequency = cacheRefreshFrequency;
-
+module.exports = (cacheRefreshFrequency) => {
 	return {
-		has,
-		read,
-		write
+		/**
+		 * Check if the cache holds an entry for the requested URL (pathname as key).
+		 * Clears outdated entries implicitly resulting in a false return value.
+		 * @param {String} key Unique key (normalize URLs)
+		 * @returns {Boolean} Whether the cache holds an entry for the requested URL
+		 */
+		has: key => {
+			if(!storage.has(key)) {
+				return false;
+			}
+			
+			return !checkEmpty(key, cacheRefreshFrequency);
+		},
+
+		/**
+		 * Read data associated with the given URL key from cache.
+		 * @param {String} key Unique key (normalize URLs)
+		 * @returns {String|Buffer} Cached data associated with the given key (undefined if not in cache)
+		 */
+		read: key => {
+			checkEmpty(key, cacheRefreshFrequency);
+			
+			if(!storage.has(key)) {
+				return undefined;
+			}
+
+			return storage.get(key).data;
+		},
+
+		/**
+		 * Write given data to the cache associated with the provided URL key.
+		 * @param {String} key Unique key (normalize URLs)
+		 * @param {String|Buffer} data Data to write to cache
+		 */
+		write: (key, data) => {
+			if(!cacheRefreshFrequency) {
+				return;
+			}
+
+			storage.set(key, {
+				time: Date.now(),
+				data: data
+			});
+		}
 	};
 };
