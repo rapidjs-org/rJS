ee1c5e2517b46f916c7067d1253f84a3efee0d54
t-ski <t.schiepanski@gmx.de>
2021-06-08 21:06:43 +0200

    Fix internal compound URL modification on root level

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,7 @@
 const {exec} = require("child_process");
 
 module.exports = {
-
-	execCommand: command => {
-		return new Promise((resolve, reject) => {
-			exec(command, (err, stdout, stderr) => {
-				if(err || stderr) {
-					reject(err || stderr);
-				}
-				
-				resolve(stdout);
-			});
-		});
-	},
-
+	
 	isString: value => {
 		return typeof value === "string" || value instanceof String;
 	},
