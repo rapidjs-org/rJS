d416b10fc80e8b88b2d3fd3078dd5aeecc04cf9b
t-ski <t.schiepanski@gmx.de>
2021-06-09 00:34:47 +0200

    Add GET request helper method to frontend core module

--- src/frontend.js
+++ src/frontend.js
@@ -1,12 +1,6 @@
-/**
- * Perform a POST request.
- * @param {String} url Endpoint URL
- * @param {Object} body Body object to send along
- * @returns {Promise} Request promise eventualy resolving to response on success
- */
-plugin.post = function(url, body) {
+function makeRequest(method, url, body = undefined) {
 	return fetch(url, {
-		method: "POST",
+		method: method,
 		mode: "same-origin",
 		credentials: "same-origin",
 		headers: {
@@ -14,6 +8,25 @@ plugin.post = function(url, body) {
 		},
 		redirect: "follow",
 		referrerPolicy: "no-referrer",
-		body: JSON.stringify(body)
+		body: body ? JSON.stringify(body) : body
 	});
+}
+
+/**
+ * Perform a GET request.
+ * @param {String} url Endpoint URL
+ * @returns {Promise} Request promise eventualy resolving to response on success
+ */
+ plugin.post = function(url) {
+	return makeRequest("GET", url);
+};
+
+/**
+ * Perform a POST request.
+ * @param {String} url Endpoint URL
+ * @param {Object} body Body object to send along
+ * @returns {Promise} Request promise eventualy resolving to response on success
+ */
+plugin.post = function(url, body) {
+	return makeRequest("POST", url, body);
 };
