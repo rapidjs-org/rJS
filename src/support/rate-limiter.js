0b35615f482110f54ea4ef8c4644c5ef15b20d64
t-ski <t.schiepanski@gmx.de>
2021-07-05 01:39:47 +0200

    Enhance rate limiter using a sliding window algorithm

--- src/support/rate-limiter.js
+++ src/support/rate-limiter.js
@@ -1,40 +1,47 @@
-// TODO: Enhance by using less trivial approach
+const limiter = {};
+let windowStart;
 
-var limiter = {};
+const PERIOD_LENGTH = 60000;
 
-// Limiter interval
-setInterval(_ => {
-	limiter = {};
-}, 60000);
+function updateWindow() {
+	const now = Date.now();
+	
+	let timeIn = Math.abs((windowStart || 0) - now);
+	
+	if(timeIn >= PERIOD_LENGTH) {
+		limiter.previous = (timeIn >= (2 * PERIOD_LENGTH)) ? {} : (limiter.current || {});
+		limiter.current = {};
 
-/**
- * Check whether to block the request by the rate limiter.
- * @param {String} ip - IP address of request's remote connection
- * @param {Number} limit - Request limit as set in config
- * @return {Boolean} - Whether the request must be blocked due to 429 (Too many requests)
- */
-function mustBlock(ip, limit) {
-	// Skip rate limiting if no limit defined or set to 0 (infinite requests allowed) in config
-	if(limit === 0) {
-		return false;
-	}
-    
-	ip = String(ip);
-	if(!limiter[ip]) {
-		limiter[ip] = 1;
-	} else {
-		limiter[ip]++;
-        
-		// Block request if is over limit
-		if(limiter[ip] > limit) {
-			return true;
-		}
+		timeIn = timeIn % PERIOD_LENGTH;
+		windowStart = now - timeIn;
 	}
 
-	// Allow request
-	return false;
+	return (timeIn / PERIOD_LENGTH);
 }
 
-module.exports = {
-	mustBlock
+/**
+ * @param {Number} limit - Request limit (as set in configuration file)
+ */
+module.exports = limit => {
+	return {
+		/**
+		 * Check whether to block the request by the rate limiter.
+		 * @param {String} ip - IP address of request's remote connection
+		 * @return {Boolean} - Whether the request must be blocked due to 429 (Too many requests)
+		 */
+		mustBlock: ip => {
+			const curWindowWeight = updateWindow();
+			const slideSum = Math.floor((curWindowWeight * (limiter.current[ip] || 0))
+							+ ((1 - curWindowWeight) * (limiter.previous[ip] || 0)));
+			if(slideSum >= limit) {
+				// Deny request
+				return true;
+			}
+
+			limiter.current[ip] = isNaN(limiter.current[ip]) ? 0 : ++limiter.current[ip];
+			
+			// Allow request
+			return false;
+		}
+	};
 };
