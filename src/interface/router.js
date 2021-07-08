0e35acf446da0f0362ddb416d755288c8339f1bd
t-ski <t.schiepanski@gmx.de>
2021-07-08 02:31:15 +0200

    Adapt router to newly introduced exclusive plug-in specific POST routing

--- src/interface/router.js
+++ src/interface/router.js
@@ -1,64 +1,40 @@
-const {normalize} = require("path");
-
-const output = require("./output");
-
 const cache = require("../support/cache");
 
-let routeHandlers = {
-	get: new Map(),
-	post: new Map()
-};
+const {getPluginName, getCallerPath} = require("../utils");
+
+const routeHandlers = new Map();
 
 module.exports =  {
 	/**
 	 * Set up a custom route handler for a certain method.
 	 * @param {String} method Name of method to bind route to
-	 * @param {String} pathname Pathname to bind route to
 	 * @param {Function} callback Callback getting passed – if applicable – the request body object eventually returning the response data to be sent
 	 * @param {Boolean} [useCache=false] Whether to cache the processed response using a server-side cache
 	 */
-	setRoute: (method, pathname, callback, useCache = false) => {
-		method = String(method).trim().toLowerCase();
+	setRoute: (callback, useCache = false) => {
+		const pathname = getPluginName(getCallerPath(__filename));
 
-		if(!["get", "post"].includes(method)) {
-			throw new SyntaxError(`${method.toUpperCase()} is not a supported HTTP method`);
-		}
-
-		pathname = normalize(pathname);
-
-		routeHandlers[method].has(pathname) && (output.log(`Redunant ${method.toUpperCase()} route handler set up for '${pathname}'`));
-
-		routeHandlers[method].set(pathname, {
+		routeHandlers.set(`/${pathname}`, {
 			callback: callback,
 			useCache: useCache
 		});
-
 		// TODO: Argument whether to apply related response modifiers to route handler response (false by default)
 	},
 
-	hasRoute: (method, pathname) => {
-		pathname = normalize(pathname);
-
-		return routeHandlers[method.toLowerCase()].has(pathname) ? true : false;
+	hasRoute: (pathname) => {
+		return routeHandlers.has(pathname) ? true : false;
 	},
 
-	applyRoute: (method, pathname, args) => {
-		if(!routeHandlers[method] || !routeHandlers[method].has(pathname)) {
-			throw new ReferenceError(`Route to be applied does not exist '${method}': '${pathname}'`);
+	applyRoute: (pathname, args) => {
+		if(routeHandlers.get(pathname).useCache && cache.has(pathname)) {
+			return cache.read(pathname);
 		}
+		
+		(args && !Array.isArray(args)) && (args = [args]);
 
-		pathname = normalize(pathname);
+		const data = routeHandlers.get(pathname).callback.apply(null, args);
+		routeHandlers.get(pathname).useCache && (cache.write(pathname, data));
 
-		let data;
-		if(routeHandlers[method].get(pathname).useCache && cache.has(pathname)) {
-			data = cache.read(pathname);
-		} else {
-			(args && !Array.isArray(args)) && (args = [args]);
-			data = routeHandlers[method].get(pathname).callback.apply(null, args);
-
-			routeHandlers[method].get(pathname).useCache && (cache.write(pathname, data));
-		}
-
-		return data || null;
+		return data;
 	}
 };
