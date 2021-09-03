a0e211d667c8cc35a877440f054a407ce467c312
t-ski <t.schiepanski@gmx.de>
2021-09-03 03:16:33 +0200

    Accumulate different interfaces file-wise using a unique shared (intersecting) model

---
+++ src/interface:app.js
@@ -0,0 +1,15 @@
+const {createInterface} = require("./utils");
+
+
+module.exports = {
+	...require("./interface:shared"),
+	...{
+		ClientError: require("./interface/ClientError"),
+		Environment: require("./interface/Environment"),
+
+		isDevMode: require("./support/is-dev-mode"),	// TODO: Improve approach?
+		
+		bindTemplating: createInterface(require("./interface/templater").bind, "binding a templating handler", true),
+		plugin: createInterface(require("./interface/plugin-management").plugin, "connecting a plug-in", true)
+	}
+};
