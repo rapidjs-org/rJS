1b545abe8e9a666e81f84248386b6052d7e1eb6d
t-ski <t.schiepanski@gmx.de>
2021-09-24 17:04:11 +0200

    Implement basic cookie manipulation interface

--- src/interface/cookies.js
+++ src/interface/cookies.js
@@ -1,5 +1,15 @@
+const entityHook = require("../server/entity-hook");
+
+//write
+
 module.exports = {
 
-    set: name => {}
+    set: (name, value) => {
+        entityHook.current().cookies[name] = value; // TODO: Store cookies
+    },
+
+    get: name => {
+        return entityHook.current().cookies[name];
+    }
 
 };
