ea67d5370e9b16a27e78547c7e7b29377f7463e2
t-ski <t.schiepanski@gmx.de>
2021-09-30 18:34:02 +0200

    Implement redirect client error status method to frontend module and preapre scope object

--- src/interface/ClientError.js
+++ src/interface/ClientError.js
@@ -5,7 +5,7 @@
 class ClientError {
 	constructor(status, message) {
 		if(!((status % 400) < 99)) {
-			throw new RangeError(`Status code ${status} not located within the client error value range (4**)`);
+			throw new RangeError(`Given status code ${status} not located within the client error value range (4**)`);
 		}
 
 		this.status = status;
