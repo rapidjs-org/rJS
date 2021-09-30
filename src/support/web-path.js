34e891dbef39f3afaf51a59769115a43e3db7603
t-ski <t.schiepanski@gmx.de>
2021-09-30 12:37:20 +0200

    Add web file directory path existance guard

--- src/support/web-path.js
+++ src/support/web-path.js
@@ -1,12 +1,21 @@
 const {join, dirname} = require("path");
+const {existsSync} = require("fs");
+
+const output = require("./output");
 
 
 const WEB_DIR_NAME = require("./web-config").webConfig.webDirectory;
 if(WEB_DIR_NAME.match(/[<>:"/\\|?*]/)) {
-    require("./output").error(
-        new SyntaxError(`"${WEB_DIR_NAME}" is not a valid directory name. Contains disallowed characters from {<, >, :, ", /, \\, ,|, ?, *}.`),
+    output.error(
+        new SyntaxError(`'${WEB_DIR_NAME}' is not a valid directory name. Contains disallowed characters from {<, >, :, ", /, \\, ,|, ?, *}.`),
         true);
 }
 
+const WEB_DIR_PATH = join(dirname(require.main.filename), WEB_DIR_NAME);
+if(!existsSync(WEB_DIR_PATH)) {
+    output.error(
+        new ReferenceError(`Web file directory does not exist at '${WEB_DIR_PATH}'`),
+        true);
+}
 
-module.exports = join(dirname(require.main.filename), WEB_DIR_NAME);
+module.exports = WEB_DIR_PATH;
