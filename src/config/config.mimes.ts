c6928aea433f0debf15b87d6d6545e36dda35f14
t-ski <t.schiepanski@gmx.de>
2022-01-06 19:13:13 +0100

    Have MIME-type config file extnesion keys be normalized and add optional, semantically more suited server config name

--- src/config/config.mimes.ts
+++ src/config/config.mimes.ts
@@ -5,7 +5,18 @@
 
 import defaultConfig from "./default/mimes.json";
 
+import {normalizeExtension} from "../utilities/normalize";
+
 import {read} from "./reader";
 
 
-export default read("mimes", defaultConfig) as Record<string, string>;
+const config = read("mimes", defaultConfig) as Record<string, string>;
+
+// Normalize extension keys
+const normalizedConfig: Record<string, string> = {};
+for(const extension in config) {
+    normalizedConfig[normalizeExtension(extension)] = config[extension]);
+}
+
+
+export default normalizedConfig;
