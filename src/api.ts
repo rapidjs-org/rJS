7744bc68ae144d6d10c93ad477aa0e6a9b04c0ca
t-ski <t.schiepanski@gmx.de>
2022-09-25 22:51:35 +0200

    Add help message text file and intial diaplay directive in entry module

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,23 @@
-// TODO: --help?
+import { readFileSync } from "fs";
+import { join } from "path";
 
-//parseArgument("help", "h")
+import { parseFlag } from "./args";
+
+
+if(parseFlag("help", "H")) {    // TODO: Global bin?
+    const helpText: string = String(readFileSync(join(__dirname, "./help.txt")));
+    console.log(helpText);
+
+    process.exit(0);
+}
+
+
+process.on("uncaughtException", (err: Error) => {
+});
+
+
+
+
+module.exports = {
+    
+};
